6dfdced80e99e8280389360be4351509a2557d34
Zachary Sidon <theplaguedoctor25@gmail.com>
2026-01-27 21:47:30 -0500

    reworked some of the code and notes

--- Lab 4/Program.cs
+++ Lab 4/Program.cs
@@ -20,30 +20,32 @@
             int hour = Int32.Parse(hourString);
             bool timeOfDay = hour > 6;
             //This is a list of variables that determine the day of the week.
-            bool dayOfWeek = day != "Monday" || day != "Tuesday" || day != "Wednesday" || day != "Thursday" || day != "Friday" || day != "Saturday" || day != "Sunday";
+            bool dayOfWeek = day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday" || day == "Saturday" || day == "Sunday";
             // STEP 3: Create an 'if' condition to check whether it is the weekend (hint: you might consider the equality operator and the OR logical operator here)
             
             // The Slection of day is determined by real time day and selection of day below.
-            if (day != "Monday" || day != "Tuesday" || day != "Wednesday" || day != "Thursday" || day != "Friday")
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday" || day == "Thursday" || day == "Friday")
             {
                 // In this Code it validates if its 6:00am or later in the week from monday to friday.
-                if (hour != 6 || hour > 6)
-                    Console.WriteLine($"It is {day} {hour} its time to wake up and get ready for the work day");
-                if (hour <= 6)
-                    Console.WriteLine($"It is {day} {hour} its not time to get up yet");   
+                if (hour == 6 || hour >= 6)
+                    Console.WriteLine($"It is {day} {hour} O' clock and it's time to wake up and get ready for the work day");
+                // This code here validates if its not 6:00am or early than 6:00am in the week from monday to friday.
+                else if (hour != 6 || hour <= 6)
+                    Console.WriteLine($"It is {day} {hour} O' clock and it's not time to get up yet");   
                 else
-                    Console.WriteLine($"It is {day} {hour} It's not time to get up, go back to sleep");
+                    Console.WriteLine($"It is {day} {hour} O' clock and it's not time to get up, go back to sleep");
             }
             // In this code it validates that these two days are on the weekends while using both weather to determine its saturday or sunday.
-            else if (day != "Saturday" || day != "Sunday")
+            else if (day == "Saturday" || day == "Sunday")
             {
                 //This Code here validates if its 9:00am or later in the week from monday to friday.
-                if (hour != 9 || hour > 9)
-                    Console.WriteLine($"It is {day} {hour} it is the weekend and it is time to wake up and enjoy the day");
-                if (hour <= 9)
-                    Console.WriteLine($"It is {day} {hour} its not time to get up yet");
+                if (hour == 9 || hour >= 9)
+                    Console.WriteLine($"It is {day} {hour} O' clock and it's is the weekend and it is time to wake up and enjoy the day");
+                //This code here validates if its not 9:00am or early than 9:00am in the week from monday to friday.
+                else if (hour != 9 || hour <= 9)
+                    Console.WriteLine($"It is {day} {hour} O' clock and it's not time to get up yet");
                 else
-                    Console.WriteLine($"It is {day} {hour} It's not time to get up go back to sleep");
+                    Console.WriteLine($"It is {day} {hour} O' clock and it's not time to get up go back to sleep");
             }
 
                 // STEP 5a: It is either Saturday or Sunday, so now we need an if/else condition to see whether it is time to wake up or not
